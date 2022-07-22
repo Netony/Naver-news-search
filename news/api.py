14ac3d6881c3cd1c93f24d9f9b7379f81bad6a72
Jeon <kuznecov98@naver.com>
2022-07-22 17:58:41 +0900

    1.0

--- news/api.py
+++ news/api.py
@@ -1,9 +1,11 @@
 import urllib.request
+import json
+from time import sleep
 
-client_id = "bIoP3NHu1eWH4ON1koub"
-client_secret = "ZBYUwa8oaT"
+client_id = "LYVZkqjtCMSsIoNTSvbo"
+client_secret = "Nd46m_umMZ"
 encText = urllib.parse.quote("검색할 단어")
-url = "https://openapi.naver.com/v1/search/blog?query=" + encText # json 결과
+url = "https://openapi.naver.com/v1/search/news?query=" + encText # json 결과
 # url = "https://openapi.naver.com/v1/search/blog.xml?query=" + encText # xml 결과
 request = urllib.request.Request(url)
 request.add_header("X-Naver-Client-Id",client_id)
@@ -12,6 +14,10 @@ response = urllib.request.urlopen(request)
 rescode = response.getcode()
 if(rescode==200):
     response_body = response.read()
-    print(response_body.decode('utf-8'))
+    data = response_body.decode('utf-8')  
+    item = json.loads(data)
+    print(type(item))
+
+    print(item)
 else:
     print("Error Code:" + rescode)
--- news/views.py
+++ news/views.py
@@ -3,34 +3,32 @@ from .models import Content
 from django.core.paginator import Paginator
 import urllib.request
 import json
-from time import sleep
+from django.utils import timezone
+
 
 def index(request):
-    Content.objects.all().delete()
     page = request.GET.get('page', '1')
     kw = request.GET.get('kw', '')
     if kw:
-        client_id = "bIoP3NHu1eWH4ON1koub"
-        client_secret = "ZBYUwa8oaT"
+        Content.objects.all().delete()
+        client_id = "LYVZkqjtCMSsIoNTSvbo"
+        client_secret = "Nd46m_umMZ"
         encText = urllib.parse.quote(kw)
-        url = "https://openapi.naver.com/v1/search/news?query=" + encText
-        request = urllib.request.Request(url)
-        request.add_header("X-Naver-Client-Id",client_id)
-        request.add_header("X-Naver-Client-Secret",client_secret)
-        response = urllib.request.urlopen(request)
-        rescode = response.getcode()
-        if(rescode==200):
-            response_body = response.read()
-            data = response_body.decode('utf-8')
-            item = json.loads(data)
-            for i in item:
-               c = Content(title=i['title'], url=i['link'], pub_date=i['pubDate'])
-               c.save()
-            sleep(5)    
-        else:
-            print("Error Code:" + rescode)
+        url = "https://openapi.naver.com/v1/search/news?display=100&query=" + encText
+        req = urllib.request.Request(url)
+        req.add_header("X-Naver-Client-Id",client_id)
+        req.add_header("X-Naver-Client-Secret",client_secret)
+        response = urllib.request.urlopen(req)
+
+        response_body = response.read()
+        data = response_body.decode('utf-8')
+        item = json.loads(data)
+        items = item['items']
+        for i in items:
+            c = Content(title=i['title'], url=i['link'], pub_date=timezone.now())
+            c.save()
     content_list = Content.objects.order_by('-pub_date')
     paginator = Paginator(content_list, 10)  # 페이지당 10개씩 보여주기
     page_obj = paginator.get_page(page)
-    context = {'content_list': page_obj}
+    context = {'content_list': page_obj, 'page': page, 'kw': kw}
     return render(request, 'news/content_list.html', context)
